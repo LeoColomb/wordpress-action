30ea68bc61c1f0ca1d1292b72db71b41d4965bf5
Léo Colombaro <git@colombaro.fr>
2019-02-17 22:09:03 +0100

    Fix shell linting

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ git archive --format=tar --prefix="archive-${TAG}/" "${TAG}" | (cd /tmp/ && tar 
 cd "/tmp/archive-${TAG}/"
 
 # Install dependencies
-if [ -f "composer.json" ] then
+if [ -f "composer.json" ]; then
   composer install --no-dev --optimize-autoloader
 fi
 
@@ -21,14 +21,14 @@ svn co "https://${WORDPRESS_TYPE}.svn.wordpress.org/${WORDPRESS_PLUGIN}/" /tmp/w
 # Remove extra artifacts
 rsync -Rrd --delete ./ /tmp/wordpress-svn/trunk/
 cd /tmp/wordpress-svn/
-svn status | grep '^!' | awk '{print $2}' | xargs -I x svn rm --force x@
+svn status | awk '/^\!/ {print $2}' | xargs svn rm --force
 
 # Add new files
-svn add --force * --auto-props --parents --depth infinity
+svn add --force ./* --auto-props --parents --depth infinity
 
 # Create the tag
-mkdir "/tmp/wordpress-svn/tags/${TAG}""
-svn add "/tmp/wordpress-svn/tags/${TAG}""
+mkdir "/tmp/wordpress-svn/tags/${TAG}"
+svn add "/tmp/wordpress-svn/tags/${TAG}"
 svn cp --parents trunk/* "tags/${TAG}"
 
 # Push the commit
